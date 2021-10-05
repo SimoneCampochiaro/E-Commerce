e652ad0a775a00106bd41eb44dada8d5fc8ea153
LukeMRL <lucamarolla@virgilio.it>
2021-10-05 16:25:09 +0200

    Update Controller

--- src/main/java/com/example/demo/controller/CarrelloController.java
+++ src/main/java/com/example/demo/controller/CarrelloController.java
@@ -1,4 +1,45 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Carrello;
+import com.example.demo.model.Clienti;
+import com.example.demo.service.CarrelloService;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class CarrelloController {
+
+    @Autowired
+
+    private CarrelloService carrelloService;
+
+    @GetMapping("/get-carrello")
+    public List<Carrello> getCarrello(){
+        return carrelloService.getCarrello();
+    }
+
+    @GetMapping("/get-carrello/{id}")
+    public Carrello getCarrelloById(@PathVariable("id") Integer id){
+        return carrelloService.getCarrelloById(id);
+    }
+
+    @PostMapping("/save-carrello")
+    public void saveDipendenti(@RequestBody @NotNull List<Carrello> carrello){
+        carrelloService.saveOrUpdateCarrello(carrello);
+    }
+
+    @DeleteMapping("/cancella-carrello")
+    public void deleteCarrello(@RequestBody @NotNull List<Carrello> carrello){
+        carrelloService.deleteCarrello(carrello);
+    }
+
+    @DeleteMapping("/cancella-carrello/{id}")
+    public void deleteCarrelloById(@PathVariable("id") Integer id){
+        carrelloService.deleteCarrelloById(id);
+    }
+
 }
--- src/main/java/com/example/demo/controller/ClientiController.java
+++ src/main/java/com/example/demo/controller/ClientiController.java
@@ -1,4 +1,44 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Clienti;
+import com.example.demo.service.ClientiService;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class ClientiController {
+
+    @Autowired
+
+    private ClientiService clientiService;
+
+    @GetMapping("/get-cliente")
+    public List<Clienti> getClienti(){
+        return clientiService.getClienti();
+    }
+
+    @GetMapping("/get-cliente/{id}")
+    public Clienti getClientiById(@PathVariable("id") Integer id){
+        return clientiService.getClientiById(id);
+    }
+
+    @PostMapping("/save-cliente")
+    public void saveClienti(@RequestBody @NotNull List<Clienti> clienti){
+        clientiService.saveOrUpdateClienti(clienti);
+    }
+
+    @DeleteMapping("/cancella-cliente")
+    public void deleteClienti(@RequestBody @NotNull List<Clienti> clienti){
+        clientiService.deleteClienti(clienti);
+    }
+
+    @DeleteMapping("/cancella-cliente/{id}")
+    public void deleteClientiById(@PathVariable("id") Integer id){
+        clientiService.deleteClientiById(id);
+    }
+
 }
