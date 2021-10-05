a6249a2bbfe2a7a9aae7a2a3edf4202bb53423d8
Simone Campochiaro <simon105@hotmail.it>
2021-10-05 17:29:53 +0200

    Controller componenti e fornitori

--- src/main/java/com/example/demo/controller/ComponentiController.java
+++ src/main/java/com/example/demo/controller/ComponentiController.java
@@ -1,4 +1,46 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.model.Clienti;
+import com.example.demo.model.Componenti;
+import com.example.demo.service.ComponentiService;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class ComponentiController {
+
+    @Autowired
+    private ComponentiService componentiService;
+
+    @GetMapping("/get-componente")
+    public List<Componenti> getFornitori() {
+        return componentiService.getComponenti();
+    }
+
+    @GetMapping("/get-componente/{id}")
+    public Componenti getComponentiById(@PathVariable("id") Integer id){
+        return componentiService.getComponentiById(id);
+    }
+
+    @PostMapping("/save-componente")
+    public void saveComponenti(@RequestBody @NotNull List<Componenti> componenti){
+        componentiService.saveOrUpdateComponenti(componenti);
+    }
+
+    @DeleteMapping("/cancella-componenti")
+    public void deleteComponenti(@RequestBody @NotNull List<Componenti> componenti){
+        componentiService.deleteComponenti(componenti);
+    }
+
+    @DeleteMapping("/cancella-componenti/{id}")
+    public void deleteComponentiById(@PathVariable("id") Integer id){
+        componentiService.deleteComponentiById(id);
+    }
+
+
 }
--- src/main/java/com/example/demo/controller/FornitoriController.java
+++ src/main/java/com/example/demo/controller/FornitoriController.java
@@ -3,10 +3,9 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Fornitori;
 import com.example.demo.service.FornitoriService;
+import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +16,28 @@ public class FornitoriController {
     @Autowired
     private FornitoriService fornitoriService;
 
-    @GetMapping("/get-fornitori")
+    @GetMapping("/get-fornitore")
     public List<Fornitori> getFornitori() {
         return fornitoriService.getFornitori();
+    }
+
+    @GetMapping("/get-fornitore/{id}")
+    public Fornitori getFornitoriById(@PathVariable("id") Integer id){
+        return fornitoriService.getFornitoriById(id);
+    }
+
+    @PostMapping("/save-fornitore")
+    public void saveFornitori(@RequestBody @NotNull List<Fornitori> fornitori){
+        fornitoriService.saveOrUpdateFornitori(fornitori);
+    }
+
+    @DeleteMapping("/cancella-fornitore")
+    public void deleteFornitori(@RequestBody @NotNull List<Fornitori> fornitori){
+        fornitoriService.deleteFornitori(fornitori);
+    }
+
+    @DeleteMapping("/cancella-fornitore/{id}")
+    public void deleteFornitoriById(@PathVariable("id") Integer id){
+        fornitoriService.deleteFornitoriById(id);
     }
 }
