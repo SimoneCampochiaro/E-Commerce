5d6fe94da28746c2e267a54b3df09da2756f7500
umbTi10 <91872910+umbTi10@users.noreply.github.com>
2021-10-05 16:51:14 +0200

    AssemblatiDAO aggiunto

--- src/main/java/com/example/demo/dao/AssemblatiDao.java
+++ src/main/java/com/example/demo/dao/AssemblatiDao.java
@@ -1,4 +1,50 @@
 package com.example.demo.dao;
 
+
+import com.example.demo.model.ComponentiAssemblato;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Repository
 public class AssemblatiDao {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public List<Assemblati> getAssemblati() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        return currentSession.createQuery("FROM Assemblati", Assemblati.class).getResultList();
+    }
+
+    public Assemblati getAssemblatiById(Integer id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Assemblati a = currentSession.find(Assemblati.class, id);
+        return a;
+    }
+
+    public void saveOrUpdateAssemblati(List<Assemblati> assemblati) {
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        for (Assemblati a : assemblati) {
+            currentSession.saveOrUpdate(a);
+        }
+    }
+
+    public void deleteAssemblati(List<Assemblati> assemblati) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        for (Assemblati a : assemblati) {
+            currentSession.delete(currentSession.find(Assemblati.class, ca.getIdAssemblati()));
+        }
+    }
+
+    public void deleteAssemblati(Integer id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.delete(currentSession.find(Assemblati.class, id));
+    }
+
 }
