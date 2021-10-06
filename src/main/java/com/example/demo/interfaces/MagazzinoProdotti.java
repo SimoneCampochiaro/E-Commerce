bc7b98d1f9aa8f86148b1d8eb9e5ce81bcf036f1
LukeMRL <lucamarolla@virgilio.it>
2021-10-06 10:23:02 +0200

    interfaccia ed eccezioni

---
+++ src/main/java/com/example/demo/interfaces/MagazzinoProdotti.java
@@ -0,0 +1,15 @@
+package com.example.demo.interfaces;
+
+import com.example.demo.model.Assemblati;
+import com.example.demo.model.Componenti;
+
+public interface MagazzinoProdotti {
+
+    void addComponenti(Componenti componenti, int quantita) throws Exception;
+
+    void addAssemblato(Assemblati assemblati, int quantita) throws Exception;
+
+    void
+
+
+}
