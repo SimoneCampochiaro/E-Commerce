49980530465242813d227053df046dee2e866b58
LukeMRL <lucamarolla@virgilio.it>
2021-10-06 10:42:18 +0200

    interfaccia ed eccezioni

--- src/main/java/com/example/demo/exception/CarrelloException.java
+++ src/main/java/com/example/demo/exception/CarrelloException.java
@@ -1,4 +1,14 @@
 package com.example.demo.exception;
 
-public class CarrelloException {
+public class CarrelloException extends Exception {
+
+    private static final long serialVersionUID = 2L;
+
+    public CarrelloException(final String message){
+        super(message);
+    }
+
+    public CarrelloException(final String message, final Throwable throwable) {
+        super(message, throwable);
+    }
 }
--- src/main/java/com/example/demo/exception/InventarioException.java
+++ src/main/java/com/example/demo/exception/InventarioException.java
@@ -1,4 +1,15 @@
 package com.example.demo.exception;
 
-public class InventarioException {
+public class InventarioException extends Exception{.
+
+    private static final long serialVersionUID = 4L;
+
+    public InventarioException(final String message) {
+        super(message);
+    }
+
+    public InventarioException(final String message, final Throwable throwable) {
+        super(message, throwable);
+    }
+
 }
