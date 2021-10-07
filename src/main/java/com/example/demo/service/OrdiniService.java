c953cd36dd7d6e6463a17cefc708ee466e16c075
LukeMRL <lucamarolla@virgilio.it>
2021-10-07 12:30:36 +0200

    Push Ordini Controller-Dao-Service

---
+++ src/main/java/com/example/demo/service/OrdiniService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.OrdiniDao;
+import com.example.demo.model.Ordini;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class OrdiniService {
+
+    @Autowired
+
+    private OrdiniDao ordiniDao;
+
+    public List<Ordini> getOrdini(){
+        return ordiniDao.getOrdini();
+    }
+
+    public Ordini getOrdiniById(Integer id){
+        return ordiniDao.getOrdiniById(id);
+    }
+
+    public void saveOrUpdateOrdini(List<Ordini> ordini){
+        ordiniDao.saveOrUpdateOrdini(ordini);
+    }
+
+    public void deleteOrdini(List<Ordini> ordini){
+        ordiniDao.deleteOrdini(ordini);
+    }
+
+    public void deleteOrdiniById(Integer id){
+        ordiniDao.deleteAOrdiniById(id);
+    }
+}
