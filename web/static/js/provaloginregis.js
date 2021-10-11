b380ce33b07251ed4ec17f205804ee2c1e5fd914
LukeMRL <lucamarolla@virgilio.it>
2021-10-11 09:35:17 +0200

    Pagine Nicola

---
+++ web/static/js/provaloginregis.js
@@ -0,0 +1,40 @@
+// sign up form
+const ToggleForm = document.querySelector('.toggle-form');
+const ToggleInner = ToggleForm.lastElementChild;
+const Login = document.getElementsByClassName('form-control')[0]
+const SignUp = document.getElementsByClassName('form-control')[1]
+
+function ChangeForm() {
+    if (ToggleInner.classList.contains('actived')) {
+        ToggleInner.classList.remove('actived')
+        Login.classList.add('logout')
+        Login.classList.remove('login-an')
+        SignUp.classList.remove('signup')
+    } else {
+        ToggleInner.classList.add('actived')
+        Login.classList.remove('logout')
+        Login.classList.add('login-an')
+        SignUp.classList.add('signup')
+    }
+}
+
+function checkResPwd(elem){
+    var pwd=document.getElementById("respwd")
+    if(pwd.value != elem.value){
+        alert("Le password non coincidono")
+        pwd.value="";
+        elem.value="";
+        return false;
+    }
+}
+
+ToggleForm.addEventListener('click', ChangeForm)
+
+const menuIcon = document.querySelector
+(".hamburger-menu");
+const navbar = document.querySelector(".navbar")
+
+menuIcon.addEventListener("click", () =>
+{
+    navbar.classList.toggle('change');
+});
